a3520a0667552aa06cec37435b0152910ca5b8f6
Avesaves <william.p.kendall@gmail.com>
2013-03-03 20:23:51 -0500

    Update LeatherBustierArms.cs
    armor buffing

--- Items/Armor/Leather/LeatherBustierArms.cs
+++ Items/Armor/Leather/LeatherBustierArms.cs
@@ -8,7 +8,7 @@ namespace Server.Items
 	{
 		public override int BaseBluntResistance{ get{ return 8; } }
 		public override int BaseSlashingResistance{ get{ return 7; } }
-		public override int BasePiercingResistance{ get{ return 4; } }
+		public override int BasePiercingResistance{ get{ return 5; } }
 		public override int BasePhysicalResistance{ get{ return 0; } }
 		public override int BaseFireResistance{ get{ return 4; } }
 		public override int BaseColdResistance{ get{ return 3; } }
