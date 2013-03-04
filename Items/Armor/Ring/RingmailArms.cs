dc23da9524a2caeeb37b01af63e921a1fa432f4e
Avesaves <william.p.kendall@gmail.com>
2013-03-03 20:39:30 -0500

    Update RingmailArms.cs
    armor buffing

--- Items/Armor/Ring/RingmailArms.cs
+++ Items/Armor/Ring/RingmailArms.cs
@@ -8,7 +8,7 @@ namespace Server.Items
 	{
 		public override int BaseBluntResistance{ get{ return 4; } }
 		public override int BaseSlashingResistance{ get{ return 8; } }
-		public override int BasePiercingResistance{ get{ return 6; } }
+		public override int BasePiercingResistance{ get{ return 7; } }
 		public override int BasePhysicalResistance{ get{ return 0; } }
 		public override int BaseFireResistance{ get{ return 3; } }
 		public override int BaseColdResistance{ get{ return 1; } }
