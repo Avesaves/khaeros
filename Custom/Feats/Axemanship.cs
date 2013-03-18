7cb1faa522b13d609d7f83651cd52f86dfc03031
Avesaves <william.p.kendall@gmail.com>
2013-03-17 21:05:24 -0300

    Update Axemanship.cs

--- Custom/Feats/Axemanship.cs
+++ Custom/Feats/Axemanship.cs
@@ -11,7 +11,7 @@ namespace Server.FeatInfo
 	{
 		public override string Name{ get{ return "Axemanship"; } }
 		public override FeatList ListName{ get{ return Mobiles.FeatList.Axemanship; } }
-		public override FeatCost CostLevel{ get{ return FeatCost.Low; } }
+		public override FeatCost CostLevel{ get{ return FeatCost.High; } }
 		
 		public override SkillName[] AssociatedSkills{ get{ return new SkillName[]{ SkillName.Axemanship }; } }
 		public override FeatList[] AssociatedFeats{ get{ return new FeatList[]{ }; } }
