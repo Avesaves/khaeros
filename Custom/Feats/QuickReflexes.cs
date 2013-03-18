8198a610b65a3d1318e6c9627363685ecf423b1e
Avesaves <william.p.kendall@gmail.com>
2013-03-17 22:18:31 -0300

    Update QuickReflexes.cs

--- Custom/Feats/QuickReflexes.cs
+++ Custom/Feats/QuickReflexes.cs
@@ -16,7 +16,7 @@ namespace Server.FeatInfo
 		public override SkillName[] AssociatedSkills{ get{ return new SkillName[]{ }; } }
 		public override FeatList[] AssociatedFeats{ get{ return new FeatList[]{ }; } }
 		
-		public override FeatList[] Requires{ get{ return new FeatList[]{ FeatList.Tactics }; } }
+		public override FeatList[] Requires{ get{ return new FeatList[]{ FeatList.Anatomy }; } }
 		public override FeatList[] Allows{ get{ return new FeatList[]{ }; } }
 		
 		public override string FirstDescription{ get{ return "Having conditioned yourself to rigorous agility training, you are now able " +
