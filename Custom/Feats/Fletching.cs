c3659c8348f606b25472e35ac3796cc9f75ffe48
Avesaves <william.p.kendall@gmail.com>
2013-03-17 21:57:12 -0300

    Update Fletching.cs

--- Custom/Feats/Fletching.cs
+++ Custom/Feats/Fletching.cs
@@ -16,7 +16,7 @@ namespace Server.FeatInfo
 		public override SkillName[] AssociatedSkills{ get{ return new SkillName[]{ SkillName.Fletching }; } }
 		public override FeatList[] AssociatedFeats{ get{ return new FeatList[]{ }; } }
 		
-		public override FeatList[] Requires{ get{ return new FeatList[]{ }; } }
+		public override FeatList[] Requires{ get{ return new FeatList[]{ FeatList.Craftsmanship }; } }
 		public override FeatList[] Allows{ get{ return new FeatList[]{ FeatList.WoodStaining }; } }
 		
 		public override string FirstDescription{ get{ return "This skill will give you some knowledge in the Fletching skill, which will " +
