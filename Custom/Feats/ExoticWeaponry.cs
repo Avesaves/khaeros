457e6490bd5a9d318ee770e8c78f40e90c72251a
Avesaves <william.p.kendall@gmail.com>
2013-03-17 21:05:07 -0300

    Update ExoticWeaponry.cs

--- Custom/Feats/ExoticWeaponry.cs
+++ Custom/Feats/ExoticWeaponry.cs
@@ -11,7 +11,7 @@ namespace Server.FeatInfo
 	{
 		public override string Name{ get{ return "Exotic Weaponry"; } }
 		public override FeatList ListName{ get{ return Mobiles.FeatList.ExoticWeaponry; } }
-		public override FeatCost CostLevel{ get{ return FeatCost.Low; } }
+		public override FeatCost CostLevel{ get{ return FeatCost.High; } }
 		
 		public override SkillName[] AssociatedSkills{ get{ return new SkillName[]{ SkillName.ExoticWeaponry }; } }
 		public override FeatList[] AssociatedFeats{ get{ return new FeatList[]{ }; } }
