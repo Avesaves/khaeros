c751c75879accf7efe39d4afa75714710e066a9c
Avesaves <william.p.kendall@gmail.com>
2013-03-17 22:36:30 -0300

    Update DecoyIII.cs

--- Custom/Spells/Custom Mage Scrolls/Level III/DecoyIII.cs
+++ Custom/Spells/Custom Mage Scrolls/Level III/DecoyIII.cs
@@ -101,7 +101,7 @@ namespace Server.Items
         {
             get
             {
-                return base.CanBeCast && HasRequiredArcanas( new FeatList[] { FeatList.ForcesII, FeatList.PrimeII, FeatList.MatterII, FeatList.SpaceII  } );
+                return base.CanBeCast && HasRequiredArcanas( new FeatList[] { FeatList.MindII } );
             }
         }
 
