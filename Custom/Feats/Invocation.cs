e8fd8f3eedfe7273548ace027d0ee2d86a68dfce
Avesaves <william.p.kendall@gmail.com>
2013-03-17 23:29:35 -0300

    Update Invocation.cs

--- Custom/Feats/Invocation.cs
+++ Custom/Feats/Invocation.cs
@@ -14,19 +14,19 @@ namespace Server.FeatInfo
 		public override FeatCost CostLevel{ get{ return FeatCost.Medium; } }
 		
 		public override SkillName[] AssociatedSkills{ get{ return new SkillName[]{ SkillName.Invocation }; } }
-		public override FeatList[] AssociatedFeats{ get{ return new FeatList[]{ FeatList.CureFamine }; } }
+		public override FeatList[] AssociatedFeats{ get{ return new FeatList[]{ }; } }
 		
 		public override FeatList[] Requires{ get{ return new FeatList[]{ }; } }
-		public override FeatList[] Allows{ get{ return new FeatList[]{ FeatList.Faith, FeatList.Magery, FeatList.CureFamine, FeatList.MagicResistance }; } }
+		public override FeatList[] Allows{ get{ return new FeatList[]{ FeatList.Faith, FeatList.Magery, FeatList.MagicResistance }; } }
 		
 		public override string FirstDescription{ get{ return "This skill will allow you to tap into the energy of Khaeros, " +
 					"give you access to more magic powers if you are worthy, and lower your enemies' chance of resisting your spells. [20% skill]"; } }
 		public override string SecondDescription{ get{ return "[50% skill]"; } }
 		public override string ThirdDescription{ get{ return "[100% skill]"; } }
 
-		public override string FirstCommand{ get{ return ".CureFamine | .CureFamine 1"; } }
-		public override string SecondCommand{ get{ return ".CureFamine | .CureFamine 2"; } }
-		public override string ThirdCommand{ get{ return ".CureFamine | .CureFamine 3"; } }
+		public override string FirstCommand{ get{ return ""; } }
+		public override string SecondCommand{ get{ return ""; } }
+		public override string ThirdCommand{ get{ return ""; } }
 		
 		public override string FullDescription{ get{ return GetFullDescription(this); } }
 		
