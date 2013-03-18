7d4040a611022ff51e9412e2431cd6060df691b7
Avesaves <william.p.kendall@gmail.com>
2013-03-17 22:31:57 -0300

    Update ExplodeCorpse.cs

--- Custom/Spells/Custom Mage Scrolls/Level I/ExplodeCorpse.cs
+++ Custom/Spells/Custom Mage Scrolls/Level I/ExplodeCorpse.cs
@@ -101,7 +101,7 @@ namespace Server.Items
         {
             get
             {                     
-                return base.CanBeCast && HasRequiredArcanas( new FeatList[]{ FeatList.ForcesI } );
+                return base.CanBeCast && HasRequiredArcanas( new FeatList[]{ FeatList.DeathI } );
             }
         }
 		
