7b674dfbd4dcd6abcb6806bb0bf1a93e59d408a2
Jere Ketonen <jere@shaedar.com>
2013-03-31 13:28:45 +0300

    Restricted military spawning to GMs.

--- Custom/Custom Guild System/CustomGuildStone.cs
+++ Custom/Custom Guild System/CustomGuildStone.cs
@@ -926,13 +926,21 @@ namespace Server.Items
 
         public static bool IsGuildMilitary(PlayerMobile m, CustomGuildStone g, bool msg)
         {
-            if (IsGuildMilitary(m, g))
-                return true;
+            if (m.AccessLevel > AccessLevel.Player)
+            {
+                if (IsGuildMilitary(m, g))
+                    return true;
+                else
+                {
+                    if (msg)
+                        m.SendMessage(
+                            "You dont have access to that function because you are not military personnel for this organization.");
+                    return false;
+                }
+            }
             else
             {
-                if (msg)
-                    m.SendMessage("You do have access to that function because you are not military personnel for this organization.");
-                return false;
+                m.SendMessage("Players do not have access to this function.");
             }
         }
 
