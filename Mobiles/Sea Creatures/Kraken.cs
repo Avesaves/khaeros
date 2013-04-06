ffe8d413f779988cb518c5422a0f61b5a4b4178c
Maarblek <spambranigan@gmail.com>
2013-04-06 02:24:20 -0300

    Increased Kraken loot

--- Mobiles/Sea Creatures/Kraken.cs
+++ Mobiles/Sea Creatures/Kraken.cs
@@ -57,6 +57,11 @@ namespace Server.Mobiles
 		public override int Hides{ get{ return 12; } }
 		public override HideType HideType{ get{ return HideType.Beast; } }
 
+		public override void GenerateLoot()
+		{
+			AddLoot( LootPack.FilthyRich);
+		}
+
 		public Kraken( Serial serial ) : base( serial )
 		{
 		}
