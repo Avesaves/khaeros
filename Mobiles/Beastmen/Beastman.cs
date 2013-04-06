34367cb5e4fd3bba6c8a8189b8e3c97fd7b20b35
Maarblek <spambranigan@gmail.com>
2013-04-06 01:24:38 -0300

    Revised Beastmen

--- Mobiles/Beastmen/Beastman.cs
+++ Mobiles/Beastmen/Beastman.cs
@@ -11,32 +11,30 @@ namespace Server.Mobiles
 		{
 			Name = NameList.RandomName( "beastman" )
 			Body = 30;
+			SetStr( 167, 195 );
+			SetDex( 26, 35 );
+			SetInt( 26, 30 );
 
-			SetDex( 66, 85 );
-			SetInt( 35 );
+			SetHits( 200, 226 );
 
-			SetHits( 176, 193 );
-
-			SetDamage( 14, 16 );
+			SetDamage( 12, 14 );
 
 			SetDamageType( ResistanceType.Blunt, 100 );
 
-			SetResistance( ResistanceType.Blunt, 30, 35 );
-			SetResistance( ResistanceType.Piercing, 30, 40 );
-			SetResistance( ResistanceType.Slashing, 30, 40 );
-			SetResistance( ResistanceType.Fire, 10, 20 );
-			SetResistance( ResistanceType.Cold, 10, 20 );
-			SetResistance( ResistanceType.Poison, 100 );
-			SetResistance( ResistanceType.Energy, 15, 25 );
+			SetResistance( ResistanceType.Blunt, 25, 35 );
+			SetResistance( ResistanceType.Slashing, 20, 30 );
+			SetResistance( ResistanceType.Piercing, 25, 35 );
+			SetResistance( ResistanceType.Energy, 25, 30 );
+			SetResistance( ResistanceType.Fire, 35, 40 );
 
-			SetSkill( SkillName.MagicResist, 50.1, 95.0 );
-			SetSkill( SkillName.Tactics, 60.1, 100.0 );
-			SetSkill( SkillName.UnarmedFighting, 60.1, 100.0 );
+			SetSkill( SkillName.MagicResist, 30.0 );
+			SetSkill( SkillName.Tactics, 80.1, 100.0 );
+			SetSkill( SkillName.UnarmedFighting, 80.1, 100.0 );
 
-			Fame = 4500;
-			Karma = -4500;
+			Fame = 8000;
+			Karma = -8000;
 
-			VirtualArmor = 34;
+			VirtualArmor = 30;
 		}
 
 		public override int GetAngerSound()
@@ -66,7 +64,7 @@ namespace Server.Mobiles
 
 		public override void GenerateLoot()
 		{
-			AddLoot( LootPack.Meager );
+			AddLoot( LootPack.Average );
 		}
 		
 		public override bool HasFur { get{ return true; } }
