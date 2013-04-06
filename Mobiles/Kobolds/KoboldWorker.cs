0e6340f2baec8c27739766adc9661f731f7fae9d
Maarblek <spambranigan@gmail.com>
2013-04-06 01:10:56 -0300

    Increased Kobold Worker loot

--- Mobiles/Kobolds/KoboldWorker.cs
+++ Mobiles/Kobolds/KoboldWorker.cs
@@ -77,6 +77,11 @@ namespace Server.Mobiles
 		{
 		}
 
+				public override void GenerateLoot()
+		{
+			AddLoot( LootPack.poor, 1 );
+		}
+
 		public override void Serialize( GenericWriter writer )
 		{
 			base.Serialize( writer );
