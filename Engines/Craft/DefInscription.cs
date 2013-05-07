3932c3878ab0be070bad8f84d6d6146eb631a1fe
Avesaves <william.p.kendall@gmail.com>
2013-05-07 03:24:12 -0300

    added music to the crafting menu

--- Engines/Craft/DefInscription.cs
+++ Engines/Craft/DefInscription.cs
@@ -93,14 +93,14 @@ namespace Server.Engines.Craft
 			AddCraft( typeof( SmallBlueBook ), "Books", "small blue book", 35.0, 55.0, typeof( Leather ), 1044462, 8, 1044463 );
 			AddCraft( typeof( SmallBrownBook ), "Books", "small brown book", 35.0, 55.0, typeof( Leather ), 1044462, 8, 1044463 );
 			AddCraft( typeof( SmallLeatherBook ), "Books", "small leather book", 30.0, 50.0, typeof( Leather ), 1044462, 12, 1044463 );
-			AddCraft( typeof( CustomSpellBook ), "Books", "spell book", 0.0, 0.0, typeof( Leather ), 1044462, 12, 1044463 );
+			//AddCraft( typeof( CustomSpellBook ), "Books", "spell book", 0.0, 0.0, typeof( Leather ), 1044462, 12, 1044463 );
 			
 			AddCraft( typeof( OldPieceOfPaper ), "Scrolls", "old piece of paper", 5.0, 25.0, typeof( Leather ), 1044462, 2, 1044463 );
 			AddCraft( typeof( OrnateScroll ), "Scrolls", "ornate scroll", 15.0, 40.0, typeof( Leather ), 1044462, 6, 1044463 );
 			AddCraft( typeof( PieceOfPaper ), "Scrolls", "piece of paper", 5.0, 30.0, typeof( Leather ), 1044462, 2, 1044463 );
 			AddCraft( typeof( RoughScroll ), "Scrolls", "rough scroll", 10.0, 30.0, typeof( Leather ), 1044462, 4, 1044463 );
 			AddCraft( typeof( AlchemicalFormula ), "Scrolls", "alchemical formula", 20.0, 40.0, typeof( Leather ), 1044462, 4, 1044463 );
-			AddCraft( typeof( CustomSpellScroll ), "Scrolls", "spell scroll", 0.0, 0.0, typeof( Leather ), 1044462, 4, 1044463 );
+			AddCraft( typeof( MusicSheet ), "Scrolls", "sheet music", 20.0, 40.0, typeof( Leather ), 1044462, 4, 1044463 );
 			
 			AddCraft( typeof( StoneTablet ), "Tablets", "stone tablet", 95.0, 102.5, typeof( Granite ), 1044514, 2, 1044513 );
 			AddCraft( typeof( RoughStoneTablet ), "Tablets", "rough stone tablet", 95.0, 102.5, typeof( Granite ), 1044514, 2, 1044513 );
