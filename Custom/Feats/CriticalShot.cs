bd94e8d17557a84d9b1b0e6449fa58b7361b056b
Malefactor <spambranigan@gmail.com>
2013-05-29 17:04:01 -0400

    Crossbow Fix, Dual Sword Nerf
    Fixed a mistake in the crossbow skill tree, added crippling shot to
    aforementioned tree, and reduced the cost of Crossbow Mastery.
    
    Removed 1 damage from Dual Swords.

--- Custom/Feats/CriticalShot.cs
+++ Custom/Feats/CriticalShot.cs
@@ -16,7 +16,7 @@ namespace Server.FeatInfo
 		public override SkillName[] AssociatedSkills{ get{ return new SkillName[]{ }; } }
 		public override FeatList[] AssociatedFeats{ get{ return new FeatList[]{ }; } }
 		
-		public override FeatList[] Requires{ get{ return new FeatList[]{ FeatList.CrossbowMastery }; } }
+		public override FeatList[] Requires{ get{ return new FeatList[]{ FeatList.BowMastery }; } }
 		public override FeatList[] Allows{ get{ return new FeatList[]{ FeatList.AimedShot }; } }
 		
 		public override string FirstDescription{ get{ return "You know where to shoot your arrows so that they really " +
