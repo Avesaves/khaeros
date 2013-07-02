eb83e4322baea5ce52a96b94a0de2be2176ac216
Jere Ketonen <jere@shaedar.com>
2013-07-02 07:24:32 +0300

    Added serialization and deserialization.

--- Spells/SpellAltar.cs
+++ Spells/SpellAltar.cs
@@ -106,6 +106,20 @@ namespace Khaeros.Scripts.Khaeros.Spells
             : base(serial)
 		{
 		}
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write((int)0); // version
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
     }
 }
 
