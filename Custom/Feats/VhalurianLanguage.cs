aff393923f362797ab80094c834f82cef4fcaca8
Avesaves <william.p.kendall@gmail.com>
2013-07-14 04:47:57 -0400

    Update VhalurianLanguage.cs

--- Custom/Feats/VhalurianLanguage.cs
+++ Custom/Feats/VhalurianLanguage.cs
@@ -20,7 +20,7 @@ namespace Server.FeatInfo
 		public override FeatList[] Allows{ get{ return new FeatList[]{ }; } }
 		
 		public override string FirstDescription{ get{ return "This skill will give you some knowledge in the Language of the Divine Union, which will " +
-					"give you a small chance, according to your skill, to understand things spoken in Vhalurian and, at max level, to speak the " +
+					"give you a small chance, according to your skill, to understand things spoken in Northern and, at max level, to speak the " +
 					"language itself. [25% chance]"; } }
 		public override string SecondDescription{ get{ return "[60% chance]"; } }
 		public override string ThirdDescription{ get{ return "[100% chance]"; } }
