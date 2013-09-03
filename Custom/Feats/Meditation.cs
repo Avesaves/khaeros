c6be2b2027e05cc6a4db59692abc5a5a24c9ab20
Avesaves <william.p.kendall@gmail.com>
2013-09-03 02:40:46 -0700

    Update Meditation.cs

--- Custom/Feats/Meditation.cs
+++ Custom/Feats/Meditation.cs
@@ -11,7 +11,7 @@ namespace Server.FeatInfo
 	{
 		public override string Name{ get{ return "Meditation"; } }
 		public override FeatList ListName{ get{ return Mobiles.FeatList.Meditation; } }
-		public override FeatCost CostLevel{ get{ return FeatCost.Low; } }
+		public override FeatCost CostLevel{ get{ return FeatCost.High; } }
 		
 		public override SkillName[] AssociatedSkills{ get{ return new SkillName[]{ SkillName.Meditation }; } }
 		public override FeatList[] AssociatedFeats{ get{ return new FeatList[]{ }; } }
