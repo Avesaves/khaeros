13f2b84474161094e89d577521772c1b79c6d87b
Maarblek <spambranigan@gmail.com>
2014-05-28 19:59:19 -0400

    New Server!

--- Misc/ServerList.cs
+++ Misc/ServerList.cs
@@ -37,7 +37,7 @@ namespace Server.Misc
 		 * firewalls) or specific IP adddresses you can do so by modifying the file SocketOptions.cs found in this directory.
 		 */
 
-		public static readonly string Address = "168.62.176.181";
+		public static readonly string Address = "74.91.112.223";
 		public static readonly string ServerName = "Khaeros";
 
 		public static readonly bool AutoDetect = true;
